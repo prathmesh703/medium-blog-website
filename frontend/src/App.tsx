5b315f213c01317516e8228ff7716a3572017d1d
prathmesh703 <146568950+prathmesh703@users.noreply.github.com>
2024-08-07 22:51:02 +0530

    frontend file created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Blog from './pages/Blog'
+
+function App() {
+ 
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/sigin' element={<Signin/>}/>
+      <Route path='/blog/:id' element={<Blog/>}/>
+    </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
